9f66dac883c97b1a4e3f4f5a572db150a37f8958
mu <59917266+4eUeP@users.noreply.github.com>
2022-12-12 16:11:52 +0800

    increasing sleep time to tmp fix tests

--- tests/test_quick_start.sh
+++ tests/test_quick_start.sh
@@ -4,6 +4,12 @@ set -e
 IMAGE=$(cat assets/quick-start.yaml | grep "image:.*hstreamdb/hstream" | head -n 1 | awk '{print $2}')
 STREAM_NAME="demo"
 
+cleanup() {
+    echo "-> clean..."
+    docker exec -t some-hstream-cli hstream --port 6570 sql -e "DROP STREAM $STREAM_NAME ;"
+    docker rm -f some-hstream-cli
+}
+
 echo "Docker image: $IMAGE"
 
 docker run -t --rm --name some-hstream-cli --network host \
@@ -13,13 +19,9 @@ docker run -td --rm --name some-hstream-cli --network host \
   $IMAGE hstream --port 6570 sql -e \
   "SELECT * FROM $STREAM_NAME WHERE humidity > 70 EMIT CHANGES;"
 
-cleanup() {
-    echo "-> clean..."
-    docker exec -t some-hstream-cli hstream --port 6570 sql -e "DROP STREAM $STREAM_NAME ;"
-    docker rm -f some-hstream-cli
-}
-
-sleep 5
+# FIXME: a SELECT task is created asynchronously. We need a way to indicate the
+# task is done instead of waiting fixed time.
+sleep 15
 
 docker exec -t some-hstream-cli hstream --port 6570 sql -e "INSERT INTO $STREAM_NAME (temperature, humidity) VALUES (22, 80);"
 docker exec -t some-hstream-cli hstream --port 6570 sql -e "INSERT INTO $STREAM_NAME (temperature, humidity) VALUES (15, 20);"
